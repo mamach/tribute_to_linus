a1ccbe791ef73ba6c1c05447ab0dedf79a31a94c
Mahesh <mahesh.chandra@adroitent.com>
2017-12-07 08:21:39 +0530

    added styles to images, body, header and footer

--- gruntfile.js
+++ gruntfile.js
@@ -1,37 +1,39 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.initConfig({
-    uglify:{
-      my_target:{
-        files:{
-          '_/js/script.js':'_/components/js/*.js'
+    uglify: {
+      my_target: {
+        files: {
+          '_/js/script.js': '_/components/js/*.js'
         }
       }
-    },//uglify
-    compass:{
-      dev:{
-        options:{
-          config:'config.rb'
+    }, //uglify
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb'
         }
       }
     },
-    watch:{
-      options:{livereload:true},
-      scripts:{
-        files:['_/components/js/*.js'],
-        tasks:['uglify']
+    watch: {
+      options: {
+        livereload: true
       },
-      sass:{
-        files:['_/components/sass/*.scss'],
-        tasks:['compass:dev']
+      scripts: {
+        files: ['_/components/js/*.js'],
+        tasks: ['uglify']
       },
-      html:{
-        files:['*.html']
+      sass: {
+        files: ['_/components/sass/*.scss'],
+        tasks: ['compass:dev']
+      },
+      html: {
+        files: ['*.html']
       }
     }
-  });//init config
+  }); //init config
   // default tasks
-  grunt.registerTask('default','watch');
-};//exports
+  grunt.registerTask('default', 'watch');
+}; //exports
